34bc1d884dd8f783712c7cb28ce811011a848e59
tumer <tumerpl@gmail.com>
2023-01-02 21:45:45 +0100

    Ticket cancel

--- src/main/java/pl/edu/pw/elka/bdbt/filharmonia/ticket/TicketController.java
+++ src/main/java/pl/edu/pw/elka/bdbt/filharmonia/ticket/TicketController.java
@@ -5,10 +5,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import pl.edu.pw.elka.bdbt.filharmonia.User;
 import pl.edu.pw.elka.bdbt.filharmonia.dao.UserDao;
 
@@ -48,4 +45,27 @@ public class TicketController {
 
     }
 
+    @PostMapping("/cancel/{id}")
+    public String cancelTicket(@PathVariable String id){
+        Authentication ctx = SecurityContextHolder.getContext().getAuthentication();
+        if(ctx.getName().equals("anonymusUser")){
+            return "403";
+        }
+
+        Long ticketId = Long.valueOf(id);
+        Optional<Ticket> ticket = ticketRepository.findById(ticketId);
+        if(ticket.isEmpty()) return "404";
+
+        if(ticket.get().getUser() == null) return "403";
+
+        User user = userDao.findUserEntityByEmail(ctx.getName());
+
+        if(!ticket.get().getUser().getEmail().equals(ctx.getName())) return "403";
+
+        ticket.get().setUser(null);
+
+        ticketRepository.save(ticket.get());
+
+        return "cancelticketsuccess";
+    }
 }
